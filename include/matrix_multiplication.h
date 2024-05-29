65e12c19b939be4ebcc416919635a5a87441f4b4
FattiMei <teomei68@gmail.com>
2024-05-29 13:43:27 +0200

    Integrate correct matrix multiplication implementation

--- include/matrix_multiplication.h
+++ include/matrix_multiplication.h
@@ -4,5 +4,7 @@
 #include <vector>
 
 void multiplyMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B, std::vector<std::vector<int>>& C, int rowsA, int colsA, int colsB);
+void multiplyMatricesWithoutErrors(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B, std::vector<std::vector<int>>& C, int rowsA, int colsA, int colsB);
+
 
 #endif // MATRIX_MULTIPLICATION_H
