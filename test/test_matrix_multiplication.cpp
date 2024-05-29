56f5cc023d52659e63ed049096d340f4e4e18098
FattiMei <teomei68@gmail.com>
2024-05-29 12:55:57 +0200

    Change spaces to tabs

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -7,28 +7,28 @@
 
 
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
-    std::vector<std::vector<int>> A = {
-        {1, 2, 3},
-        {4, 5, 6}
-    };
-    std::vector<std::vector<int>> B = {
-        {7, 8},
-        {9, 10},
-        {11, 12}
-    };
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+	std::vector<std::vector<int>> A = {
+		{1, 2, 3},
+		{4, 5, 6}
+	};
+	std::vector<std::vector<int>> B = {
+		{7, 8},
+		{9, 10},
+		{11, 12}
+	};
+	std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
 
-    multiplyMatrices(A, B, C, 2, 3, 2);
+	multiplyMatrices(A, B, C, 2, 3, 2);
 
-    std::vector<std::vector<int>> expected = {
-        {58, 64},
-        {139, 154}
-    };
+	std::vector<std::vector<int>> expected = {
+		{58, 64},
+		{139, 154}
+	};
 
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+	ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
