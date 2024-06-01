b2a68bfaa14cd3b13b8160d868598223f2c6a8f9
mattecenz <matteo.cenzato@mail.polimi.it>
2024-06-02 00:00:34 +0200

    added last 10 tests

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -81,6 +81,9 @@ TEST(MatrixMultiplicationTest, Error1Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	//Also assert expected dimensions
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -112,6 +115,8 @@ TEST(MatrixMultiplicationTest, Error2Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -143,6 +148,8 @@ TEST(MatrixMultiplicationTest, Error3Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -172,6 +179,8 @@ TEST(MatrixMultiplicationTest, Error4Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -199,6 +208,8 @@ TEST(MatrixMultiplicationTest, Error5Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -226,6 +237,8 @@ TEST(MatrixMultiplicationTest, Error6Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -253,6 +266,8 @@ TEST(MatrixMultiplicationTest, Error7Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -282,6 +297,8 @@ TEST(MatrixMultiplicationTest, Error8Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -309,6 +326,8 @@ TEST(MatrixMultiplicationTest, Error9Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
@@ -336,19 +355,348 @@ TEST(MatrixMultiplicationTest, Error10Test){
 
 	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
 
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
 	ASSERT_NE(C,E);
 
 }
 	// Error 11: Every row in matrix B contains at least one '0'!
+TEST(MatrixMultiplicationTest, Error11Test){
+
+	Matrix A = {
+
+		{2,9},
+
+	};
+
+	Matrix B = {
+
+		{1,0},
+		{0,4}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{2,36}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 12: The number of rows in A is equal to the number of columns in B!
+TEST(MatrixMultiplicationTest, Error12Test){
+
+	Matrix A = {
+
+		{2,4},
+
+	};
+
+	Matrix B = {
+
+		{1},
+		{8}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{34}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 13: The first element of matrix A is equal to the first element of matrix B!
+TEST(MatrixMultiplicationTest, Error13Test){
+
+	Matrix A = {
+
+		{2,6},
+
+	};
+
+	Matrix B = {
+
+		{2,2},
+		{8,4}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{52,28}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 14: The result matrix C has an even number of rows!
+TEST(MatrixMultiplicationTest, Error14Test){
+
+	//NB: this will print it two times but it is not possible to print it only once
+
+	Matrix A = {
+
+		{2,6},
+		{3,4},
+		{2,6},
+		{3,4}
+	};
+
+	Matrix B = {
+
+		{1,2,4},
+		{8,4,5},
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{50,28,38},
+		{35,22,32},
+		{50,28,38},
+		{35,22,32}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 15: A row in matrix A is filled entirely with 5s!
+TEST(MatrixMultiplicationTest, Error15Test){
+
+	Matrix A = {
+
+		{5,5},
+
+	};
+
+	Matrix B = {
+
+		{4,2},
+		{2,4}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{30,30}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 16: Matrix B contains the number 6!
+TEST(MatrixMultiplicationTest, Error16Test){
+
+	Matrix A = {
+
+		{2,9},
+
+	};
+
+	Matrix B = {
+
+		{1,0},
+		{6,4}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{56,36}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 17: Result matrix C contains the number 17!
+TEST(MatrixMultiplicationTest, Error17Test){
+
+	// So here since is pseudo-randomically generated we kinda have to be lucky
+	// that the rand() function is equal in every architecture we are testing.
+	// Or else this test is actually infeasible
+	srand(16); // <- leave it like this, it took me a lot of time to find it
+
+	// Here the test will always produce also the error 7
+
+	Matrix A = {
+
+		{2,5},
+
+	};
+
+	Matrix B = {
+
+		{1,1},
+		{4,2}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{22,12}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 18: Matrix A is a square matrix!
+TEST(MatrixMultiplicationTest, Error18Test){
+
+	// NB: Error 14 => matrix A has an even number of rows and columns (since square)
+	//     Error 20 => matrix A has an odd number of rows (since square) and columns
+
+	// We have to catch at least one of these two additional errors then
+
+	Matrix A = {
+
+		{2}
+
+	};
+
+	Matrix B = {
+
+		{1,4},
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{2,8}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 19: Every row in matrix A contains the number 8!
+TEST(MatrixMultiplicationTest, Error19Test){
+
+	Matrix A = {
+
+		{8,1},
+
+	};
+
+	Matrix B = {
+
+		{2,4},
+		{5,4}
+		
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{21,36}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
 	// Error 20: Number of columns in matrix A is odd!
+TEST(MatrixMultiplicationTest, Error20Test){
+
+	Matrix A = {
+
+		{2,9,5},
+
+	};
+
+	Matrix B = {
+
+		{1,4},
+		{2,2},
+		{5,4}
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{45,46},
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	ASSERT_NE(C,E);
+
+}
+
+TEST(MatrixMultiplicationTest, NoErrorTest){
+	
+	Matrix A = {
+		{4,4},
+		{2,8},
+		{5,6}
+	};
+
+	Matrix B = {
+		{2,5,4,5},
+		{7,7,8,9}
+	};
+
+	Matrix C(A.size(),std::vector<int>(B[0].size(),0));
+
+	Matrix E = {
+		{36,48,48,56},
+		{60,66,72,82},
+		{52,67,68,79}
+	};
+
+	multiplyMatrices(A,B,C,A.size(),A[0].size(),B[0].size());
+
+	ASSERT_EQ(C.size(),E.size());
+	ASSERT_EQ(C[0].size(),E[0].size());
+	// Here we check the equality
+	ASSERT_EQ(C,E);
+}
 
 int main(int argc, char **argv) {
 	testing::InitGoogleTest(&argc, argv);
