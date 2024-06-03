55c4e96615089e7d1d4e4c7163cdca9bbbde81e9
FattiMei <teomei68@gmail.com>
2024-06-03 09:19:50 +0200

    bugfix: wrong executable path

--- error_tracker.sh
+++ error_tracker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./build/test_multiplication &> tmp
+$1 &> tmp
 grep "^Error:*" tmp | awk '{print $2, $i}' | sort -n | uniq | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}'
 rm tmp
